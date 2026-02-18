64f3f20bf6b310bdd138d3717f6ca24f49ae378a
이경용 <sleekydz86@icloud.com>
2026-02-18 18:16:22 +0900

    feat: 프로젝트 생성

---
+++ namuwikingestion/src/main/kotlin/com/sleekydz86/namuwikingestion/NamuwikingestionApplication.kt
@@ -0,0 +1,11 @@
+package com.sleekydz86.namuwikingestion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NamuwikingestionApplication
+
+fun main(args: Array<String>) {
+    runApplication<NamuwikingestionApplication>(*args)
+}
